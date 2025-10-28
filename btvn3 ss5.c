6019d416f1389a6de323c409ad8516c1965bf4a6
khanh23K <khanhkinh23@gmail.com>
2025-10-28 22:43:11 +0700

    Add files via upload

--- btvn3 ss5.c
+++ btvn3 ss5.c
@@ -3,7 +3,7 @@ int main(){
 	float n;
 	printf("nhap diem cua ban: ");
 	scanf("%f",&n);
-	if(n >= 6.5){
+	if(n >= 8.0){
 		printf("ban la Hoc sinh gioi");
 	}else if(n >=5.0){
 		printf("ban la Hoc sinh kha");
